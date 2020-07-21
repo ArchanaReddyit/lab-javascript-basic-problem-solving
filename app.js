f964d5012b5bede42eb7a7a66cce15c128f53314
ArchanaReddyit <archanareddy.it@gmail.com>
2020-07-21 22:35:55 +0400

    done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Important Note - No Built-in functions to be used
 // Progression 1: Names and Input
+function print() {
+    var ProGrad1 = "The driver's name is ArchanaReddy";
+    console.log(ProGrad1);
+    var ProGrad2 = "The navigator's name is Nikhil";
+    console.log(ProGrad2);
+}
+
 
 // 1.1 Create a variable `ProGrad-1` with the driver's name.
 // 1.2 Print `"The driver's name is XXXX"`.
@@ -7,30 +14,127 @@
 // 1.4 Print `"The navigator's name is YYYY"`.
 
 // Progression 2: Control Statements - 1
+
+function char() {
+    const str = 'ArchanaReddy';
+    const str1 = 'Nikhil';
+
+    let a = str.length;
+    let b = str1.length;
+
+    if (a > b) {
+        console.log(str);
+    } else {
+        console.log(str1)
+    }
+}
+char();
+
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
+
+
+
+function getVowels(str) {
+    var vowelsCount = 0;
+    //turn the input into a string
+    var string = str.toString();
+    //  var str = "ArchanaReddy";
+    //loop through the string
+    for (var i = 0; i <= string.length - 1; i++) {
+        //if a vowel, add to vowel count
+        if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+            vowelsCount += 1;
+            console.log(string.charAt(i));
+            // console.log(str);
+        }
+    }
+    return vowelsCount;
+}
+console.log(getVowels("ArchanaReddy")); // 2 a e
 
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the name, and also print the vowel letters along with the vowel index. or
 // - print no vowels
 // - for example. In String ProGrad - o and a are vowels. Print ProGrad o a 2 5. 
 
+function Case() {
+    let name = "ArchanaReddy";
+    //let len;
+    var uppercase = 0;
+    var lowercase = 0;
+    for (var i = 0; i < name.length; i++) {
+        if (name[i] >= "a" && name[i] <= "z")
+            lowercase++;
+        else
+            uppercase++;
+    }
+
+    console.log("uppercase in " + name + "  " + uppercase);
+    console.log("lowercase in " + name + " " + lowercase);
+}
+
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
 
 
+
 // Progression 3: Control Statements - 2
-// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
+function printchar() {
+    var ProGrad1 = "ArchanaReddy";
+    var ProGrad2 = "Nikhil";
+    //  3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
 
-// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+    var input = ProGrad1.split("");
+    var str = " ";
+    for (let i = 0; i < input.length; i++) {
+        str = str + input[i].toUpperCase() + " ";
+    }
+    console.log(str);
 
-// 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
-// - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
+
+    // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+    // var ProGrad1 = "ArchanaReddy";
+
+    // function reverseString(ProGrad1) {
+    //     console.log(ProGrad1.split("").reverse().join(""))
+    //     // return ProGrad1.split("").reverse().join("");
+    // }
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+    console.log(reverseString('ProGrad'));
+
+    // console.log(ProGrad1);
+    // console.log(ProGrad2);
+
+    var input = ProGrad1.split("");
+    var str = " ";
+    for (let i = 0; i < input.length; i++) {
+        str = str + input[i].toUpperCase() + " ";
+        // str.split("").reverse().log();
+    }
+    console.log(str);
+
+    // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
+    // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
+    console.log(ProGrad1.concat(" ", ProGrad2));
+    console.log(ProGrad2.concat(" ", ProGrad1));
+}
+
 
 // 3.3 Depending on the lexicographic order of the strings, print:
+function lexicographic() {
+    var elements = ['Nikhil', 'ArchanaReddy'];
+    a = elements.sort((a, b) => a.localeCompare(b));
+
+    // Returning sorted elements 
+    console.log(a);
+}
+
 // - The driver's name goes first.
 // - Yo, the navigator goes first definitely.
 // - What?! You both have the same name?
